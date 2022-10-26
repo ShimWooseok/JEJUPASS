6299d79bed7b1522c50231bee698ec117ca5d5b6
swss <zaqplm7913@naver.com>
2022-10-26 15:48:16 +0900

    메세지

--- jss.js
+++ jss.js
@@ -16,6 +16,28 @@ $(document).ready(function(){
         $(".wrapper > .hidden-box").removeClass("active");
     })
 
+
+
+    $(".wrapper > .scroll-down-top-bar > .con > .side-menu").click(function(){
+
+        $(".wrapper > .side").addClass("active");
+        $(".wrapper > .hidden-box").addClass("active");
+        
+
+    })
+    $(".wrapper > .side > div > .top > .exit").click(function(){
+        $(".wrapper > .side").removeClass("active");
+        $(".wrapper > .hidden-box").removeClass("active");
+    })
+    $(".wrapper > .hidden-box").click(function(){
+        $(".wrapper > .side").removeClass("active") ;
+        $(".wrapper > .hidden-box").removeClass("active");
+    })
+
+
+
+
+
     $(".section01 > .con > .menual > div > img").click(function(){
 
         let num = $(this).index();
@@ -28,3 +50,14 @@ $(document).ready(function(){
 
     // 끝
 })
+
+
+$(window).scroll(function(){
+    let scrollTop = $(this).scrollTop()
+    
+    if(scrollTop>=200){
+      $(".scroll-down-top-bar").addClass("active")
+    } else if(scrollTop < 200){
+      $(".scroll-down-top-bar").removeClass("active")
+    }
+  })
