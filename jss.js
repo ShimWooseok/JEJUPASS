daeb7756f418c6a09b33edd749bae851f6a404c7
swss <zaqplm7913@naver.com>
2022-11-14 11:11:54 +0900

    2022.11.14

--- jss.js
+++ jss.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
       // centeredSlides: false,
       spaceBetween: 30,
       autoplay : {
-        delay : 2000
+        delay : 3000
       },
       loop : true,
       pagination: {
